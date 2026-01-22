0f3c83b9b6978a94e1e8107f49f166cb178b5454
Charcoalie06 <charliekock06@gmail.com>
2026-01-22 14:02:24 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import static java.lang.System.out;
-
-public class Main {
-    public static void main(String[] args) {
-        Saab95 saab = new Saab95();
-        out.println(saab.enginePower);
-    }
-}
