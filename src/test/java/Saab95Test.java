ccd48fc3230313cb5ffa2607ce77d61be54ef337
Charcoalie06 <charliekock06@gmail.com>
2026-01-27 10:53:38 +0100

    saab tests file

---
+++ src/test/java/Saab95Test.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Saab95Test {
+    private final Saab95 saab95 = new Saab95();
+
+    @Test
+    void setTurboOn() {
+    }
+
+    @Test
+    void setTurboOff() {
+    }
+
+    @Test
+    void incrementSpeed() {
+    }
+
+    @Test
+    void decrementSpeed() {
+    }
+}
