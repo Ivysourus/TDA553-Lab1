2f1c6b5ce908b18d989e90caedcacef864215a86
Charcoalie06 <charliekock06@gmail.com>
2026-01-27 10:37:00 +0100

    convert to maven project and first tets

---
+++ src/test/java/Volvo240Test.java
@@ -0,0 +1,55 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Volvo240Test {
+    private final Volvo240 volvo240 = new Volvo240();
+
+    @Test
+    void getNrDoors() {
+        assertEquals(4, volvo240.getNrDoors());
+    }
+
+    @Test
+    void getEnginePower() {
+    }
+
+    @Test
+    void getCurrentSpeed() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void startEngine() {
+    }
+
+    @Test
+    void stopEngine() {
+    }
+
+    @Test
+    void gas() {
+    }
+
+    @Test
+    void brake() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void turnLeft() {
+    }
+
+    @Test
+    void turnRight() {
+    }
+
+    @Test
+    void speedFactor() {
+    }
+}
