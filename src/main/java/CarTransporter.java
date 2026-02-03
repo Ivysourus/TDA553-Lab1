cd060f0fba422c53886eec07e7b3ef8a097696da
ivarnors <ivarnors@chalmers.se>
2026-02-03 15:17:45 +0100

    Added Override move method.

--- src/main/java/CarTransporter.java
+++ src/main/java/CarTransporter.java
@@ -49,6 +49,14 @@ public class CarTransporter extends Car implements CanLoadOrdered<PersonCar>  {
     }
 
     @Override
+    public void move() {
+        super.move();
+        for (int i = 0; i < canLoadHelper.cargo.size(); i++){
+            canLoadHelper.cargo.get(i).pos = this.pos;
+        }
+    }
+
+    @Override
     protected double speedFactor() {
         return enginePower * 0.01;
     }
