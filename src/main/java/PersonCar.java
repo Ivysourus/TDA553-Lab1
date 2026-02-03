cca691d7d0c86750733cf4898de5041096c21bb7
Love Lysell Berglund <lovistovis0@gmail.com>
2026-02-03 14:53:43 +0100

    CarFerry progress and make vehicle loadable

--- src/main/java/PersonCar.java
+++ src/main/java/PersonCar.java
@@ -1,9 +1,1 @@
-public abstract class PersonCar extends Car implements Loadable {
-    public void setPosition(Vector2 pos) {
-        this.pos = pos;
-    }
-
-    public Vector2 getPosition() {
-        return new Vector2(pos);
-    }
-}
+public abstract class PersonCar extends Car {}
