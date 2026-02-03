be93880a31b4f7c74f50be962ca6dbe1b8afa5d9
Love Lysell Berglund <lovistovis0@gmail.com>
2026-02-03 13:34:44 +0100

    formatting

--- src/test/java/ScaniaTest.java
+++ src/test/java/ScaniaTest.java
@@ -10,21 +10,21 @@ class ScaniaTest {
     @Test
     void raiseBedAtZero() {
         double initial = scania.getBedAngle();
-        double angle = 0.1*Math.PI;
+        double angle = 0.1 * Math.PI;
         scania.raiseBed(angle);
-        assertEquals(scania.getBedAngle(), initial+angle);
+        assertEquals(scania.getBedAngle(), initial + angle);
     }
 
     void lowerBedAtZero() {
         double initial = scania.getBedAngle();
-        double angle = 0.1*Math.PI;
+        double angle = 0.1 * Math.PI;
         scania.lowerBed(angle);
         assertEquals(scania.getBedAngle(), 0);
     }
 
     void raiseBedMaxLimit() {
-        double angle = 2.0*Math.PI;
+        double angle = 2.0 * Math.PI;
         scania.raiseBed(angle);
-        assertEquals(scania.getBedAngle(), (7.0/18.0)*Math.PI);
+        assertEquals(scania.getBedAngle(), (7.0 / 18.0) * Math.PI);
     }
 }
