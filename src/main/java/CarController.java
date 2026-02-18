908d749b7fa2e6ecd5761ae7861c200e6f1ea0ac
Love Lysell Berglund <love.lysell.berglund@gmail.com>
2026-02-18 14:19:20 +0100

    unnecessary comments

--- src/main/java/CarController.java
+++ src/main/java/CarController.java
@@ -10,8 +10,6 @@ import javax.swing.Timer;
 * modifying the model state and the updating the view.
  */
 public class CarController {
-    // member fields:
-
     // The delay (ms) corresponds to 20 updates a sec (hz)
     private final int delay = 50;
     // The timer is started with a listener (see below) that executes the statements
@@ -20,22 +18,17 @@ public class CarController {
 
     // The frame that represents this instance View of the MVC pattern
     CarView frame;
-    // A list of cars, modify if needed
     ArrayList<EngineVehicle> cars = new ArrayList<>();
-    // methods:
 
     public static void main(String[] args) {
-        // Instance of this class
         CarController cc = new CarController();
 
         cc.cars.add(new Volvo240());
-        // cc.cars.add(new Scania());
-        // cc.cars.add(new Saab95());
+        cc.cars.add(new Scania());
+        cc.cars.add(new Saab95());
 
-        // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
 
-        // Start the timer
         cc.timer.start();
     }
 
@@ -56,7 +49,6 @@ public class CarController {
         }
     }
 
-    // Calls the gas method for each car once
     void gas(int amount) {
         double gas = ((double) amount) / 100;
         for (EngineVehicle car : cars) {
