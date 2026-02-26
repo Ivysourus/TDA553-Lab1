00cf0bfb7155b72fc0fdae293608e73f78788c83
Love Lysell Berglund <love.lysell.berglund@gmail.com>
2026-02-26 10:50:42 +0100

    factory fix

--- src/main/java/org/tda553group22/lab3/core/CanLoadUnorderedFactory.java
+++ src/main/java/org/tda553group22/lab3/core/CanLoadUnorderedFactory.java
@@ -3,8 +3,7 @@ package org.tda553group22.lab3.core;
 import org.tda553group22.lab3.math.Vector2;
 
 public final class CanLoadUnorderedFactory {
-
-    public static CanLoadUnordered<Volvo240> createVolvoWorkshop(int capacity, Vector2 pos) {
-        return new WorkshopWithPosition<>(capacity, pos);
+    public static CanLoadUnordered<Volvo240> createVolvoWorkshop(Vector2 pos, int capacity) {
+        return new Workshop<>(pos, capacity);
     }
 }
