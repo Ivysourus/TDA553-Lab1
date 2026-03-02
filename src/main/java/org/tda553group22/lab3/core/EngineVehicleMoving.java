95678108b23b7af7d23dcb7b4125b26cd3247863
ivarnors <ivarnors@chalmers.se>
2026-03-02 20:49:11 +0100

    Added Overrides

--- src/main/java/org/tda553group22/lab3/core/EngineVehicleMoving.java
+++ src/main/java/org/tda553group22/lab3/core/EngineVehicleMoving.java
@@ -1,10 +1,12 @@
 package org.tda553group22.lab3.core;
 
 public class EngineVehicleMoving implements EngineVehicleState {
+    @Override
     public void startEngine(EngineVehicle vehicle){
         // Do nothing
     }
 
+    @Override
     public void stopEngine(EngineVehicle vehicle){
         vehicle.currentSpeed = 0;
         vehicle.state = new EngineVehicleStopped();
@@ -15,6 +17,7 @@ public class EngineVehicleMoving implements EngineVehicleState {
      *
      * @param amount The amount to speed up by in the range [0,1].
      */
+    @Override
     public void gas(EngineVehicle vehicle, double amount){
         assert amount >= 0.0 && amount <= 1.0 : "Variable `amount` outside the range [0,1]";
         vehicle.incrementSpeed(amount);
@@ -25,6 +28,7 @@ public class EngineVehicleMoving implements EngineVehicleState {
      *
      * @param amount The amount to slow down by in the range [0,1].
      */
+    @Override
     public void brake(EngineVehicle vehicle, double amount){
         assert amount >= 0.0 && amount <= 1.0 : "Variable `amount` outside the range [0,1]";
         vehicle.decrementSpeed(amount);
--- src/main/java/org/tda553group22/lab3/core/EngineVehicleStopped.java
+++ src/main/java/org/tda553group22/lab3/core/EngineVehicleStopped.java
@@ -1,19 +1,23 @@
 package org.tda553group22.lab3.core;
 
 public class EngineVehicleStopped implements EngineVehicleState {
+    @Override
     public void startEngine(EngineVehicle vehicle){
         vehicle.currentSpeed = 0.1;
         vehicle.state = new EngineVehicleMoving();
     }
 
+    @Override
     public void stopEngine(EngineVehicle vehicle){
         // Do nothing
     }
 
+    @Override
     public void gas(EngineVehicle vehicle, double amount){
         // Do nothing
     }
 
+    @Override
     public void brake(EngineVehicle vehicle, double amount){
         // Do nothing
     }
